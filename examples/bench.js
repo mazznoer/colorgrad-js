1047a0a3d225110e1e7f2670de6ecf3aa27f2dac
Nor Khasyatillah <mazznoer@ymail.com>
2022-07-28 20:16:12 +0700

    add benchmark

---
+++ examples/bench.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import benchmark from "benchmark";
+import colorgrad from "@mazznoer/colorgrad";
+import chroma from "chroma-js";
+import tinygradient from "tinygradient";
+import interpolate from "color-interpolate";
+
+const g_colorgrad = colorgrad.custom_gradient(["red", "green", "blue"]);
+const g_chroma = chroma.scale(["red", "green", "blue"]);
+const g_tinygradient = tinygradient(["red", "green", "blue"]);
+const g_color_interpolate = interpolate(["red", "green", "blue"]);
+
+//console.log("@mazznoer/colorgrad", g_colorgrad.at(0.75).hex());
+//console.log("chroma", g_chroma(0.75).hex());
+//console.log("tinygradient", g_tinygradient.rgbAt(0.75).toHex());
+//console.log("color-interpolate", g_color_interpolate(0.75));
+
+let suite = new benchmark.Suite;
+
+suite.add("@mazznoer/colorgrad", function() {
+        g_colorgrad.at(0.75);
+    })
+    .add("chroma-js", function() {
+        g_chroma(0.75);
+    })
+    .add("tinygradient", function() {
+        g_tinygradient.rgbAt(0.75);
+    })
+    .add("color-interpolate", function() {
+        g_color_interpolate(0.75);
+    })
+    .on("cycle", function(event) {
+        console.log(String(event.target));
+    })
+    .on("complete", function() {
+        console.log("Fastest is " + this.filter("fastest").map("name"));
+    })
+    .run({
+        "async": true
+    });
