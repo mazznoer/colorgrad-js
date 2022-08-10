146a6863bbaabc425375d88b9b8cf1c0ffb5a6b5
Nor Khasyatillah <mazznoer@ymail.com>
2022-08-11 01:22:31 +0700

    update benchmark

--- examples/bench.js
+++ examples/bench.js
@@ -1,31 +1,44 @@
 #!/usr/bin/env node
 
 import benchmark from "benchmark";
-import colorgrad from "@mazznoer/colorgrad";
+import colorgrad from "colorgrad-js";
 import chroma from "chroma-js";
+import * as culori from "culori";
 import * as d3_scale from "d3-scale";
 import tinygradient from "tinygradient";
 import interpolate from "color-interpolate";
 
-const g_colorgrad = colorgrad.custom_gradient(["red", "green", "blue"]);
-const g_chroma = chroma.scale(["red", "green", "blue"]);
-const g_d3_scale = d3_scale.scaleLinear([0, 0.5, 1], ["red", "green", "blue"]);
-const g_tinygradient = tinygradient(["red", "green", "blue"]);
-const g_color_interpolate = interpolate(["red", "green", "blue"]);
+const colors = [
+    "#e830d8", "#bf6242", "#a0c433", "#8299d8", "#e5ca34", "#96d811", "#afffe3", "#079988",
+    "#c0e88d", "#ed703b", "#233b8c", "#ede17b", "#6dea0e", "#30e8d5", "#8fc7fc", "#1ee824",
+    "#38ea2e", "#fcc4d5", "#529915", "#c8f280", "#ffc4db", "#cae85f", "#ba1f62", "#60e0da",
+];
 
-//console.log("@mazznoer/colorgrad", g_colorgrad.at(0.75).hex());
-//console.log("chroma", g_chroma(0.75).hex());
-//console.log("d3-scale", g_d3_scale(0.75));
-//console.log("tinygradient", g_tinygradient.rgbAt(0.75).toHex());
-//console.log("color-interpolate", g_color_interpolate(0.75));
+const g_colorgrad = colorgrad.customGradient(colors);
+const g_chroma = chroma.scale(colors);
+const g_culori = culori.interpolate(colors);
+const g_d3_scale = d3_scale.scaleLinear([0, 0.5, 1], colors);
+const g_tinygradient = tinygradient(colors);
+const g_color_interpolate = interpolate(colors);
+/*
+console.log("colorgrad", g_colorgrad.at(0.75).hex());
+console.log("chroma", g_chroma(0.75).hex());
+console.log("culori", culori.formatHex(g_culori(0.75)));
+console.log("d3-scale", g_d3_scale(0.75));
+console.log("tinygradient", g_tinygradient.rgbAt(0.75).toHex());
+console.log("color-interpolate", g_color_interpolate(0.75));*/
 
-let suite = new benchmark.Suite;
+const suite = new benchmark.Suite;
 
-suite.add("@mazznoer/colorgrad", function() {
+suite
+    .add("colorgrad-js", function() {
         g_colorgrad.at(0.75);
     })
     .add("chroma-js", function() {
         g_chroma(0.75);
+    })
+    .add("culori", function() {
+        g_culori(0.75);
     })
     .add("d3-scale", function() {
         g_d3_scale(0.75);
